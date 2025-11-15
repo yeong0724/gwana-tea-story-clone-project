4a481ea72fa6c1bd93fb1c5535b4e03b8d83b59a
Jinyeong Kim <kid4211@gmail.com>
2025-11-15 20:39:40 +0900

    상품 이미지 추가 - 2

--- next.config.ts
+++ next.config.ts
@@ -3,16 +3,7 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'via.placeholder.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'placehold.co',
-      },
-    ],
+    remotePatterns: [],
   },
 };
 
